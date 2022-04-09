0ede44b7403589b9ea4d3fa5cc8b43f1497b45bb
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-04-10 06:54:07 +0930

    javascript folded

---
+++ javascripts/gsap.js
@@ -0,0 +1,3 @@
+/* ----------------------------------------- Gsap animation added ---------------------------------------- */
+gsap.from('.nav', {duration: 1, y: '-100%', ease:'bounce'})
+gsap.from('.nav__item', {duration: 1, opacity: 0, delay: 1})
