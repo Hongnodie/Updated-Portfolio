2f79654f137e6dde0c0db35bcff704392045fdc7
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-04-10 07:37:07 +0930

    nav bar bounce in

--- javascripts/gsap.js
+++ javascripts/gsap.js
@@ -1,3 +1,6 @@
 /* ----------------------------------------- Gsap animation added ---------------------------------------- */
-gsap.from('.nav', {duration: 1, y: '-100%', ease:'bounce'})
-gsap.from('.nav__item', {duration: 1, opacity: 0, delay: 1})
+gsap.from('.nav__items', {duration: 1, y: '-100%', delay: 2.5, ease: "elastic"})
+
+gsap.from('.nav__item', {duration: 1, opacity: 0, delay: 3.5, stagger: .2})
+
+gsap.from('.header__text', {duration: 2.5, x: '-100%', ease: "power2.out"})
