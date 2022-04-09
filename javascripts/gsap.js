d158e2c25483d44ca8692b9fe56755927120dcfe
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-04-10 07:55:57 +0930

    header background image

--- javascripts/gsap.js
+++ javascripts/gsap.js
@@ -1,5 +1,5 @@
 /* ----------------------------------------- Gsap animation added ---------------------------------------- */
-gsap.from('.nav__items', {duration: 1, y: '-100%', delay: 2.5, ease: "elastic"})
+gsap.from('.nav__items', {duration: 1, y: '-200%', delay: 2.5, ease: "elastic"})
 
 gsap.from('.nav__item', {duration: 1, opacity: 0, delay: 3.5, stagger: .2})
 
