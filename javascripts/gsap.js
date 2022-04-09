414257563f7d96574ec8ead9e08e0b80be14c8b8
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-04-10 08:06:08 +0930

    btn rotate in

--- javascripts/gsap.js
+++ javascripts/gsap.js
@@ -4,3 +4,5 @@ gsap.from('.nav__items', {duration: 1, y: '-200%', delay: 2.5, ease: "elastic"})
 gsap.from('.nav__item', {duration: 1, opacity: 0, delay: 3.5, stagger: .2})
 
 gsap.from('.header__text', {duration: 2.5, x: '-100%', ease: "power2.out"})
+
+gsap.fromTo('.btn', {opacity: 0, sacle: 0, rotation: 360}, {duration: .8, delay: 2.5, opacity: 1, sacle: 1, rotation: 0})
