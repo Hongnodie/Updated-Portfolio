3c79bd9cb428fa346d2ad28020f5130ad1aa7b41
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-04-11 05:39:25 +0930

    Seperated indi and team work

--- javascripts/gsap.js
+++ javascripts/gsap.js
@@ -5,57 +5,68 @@ gsap.from('.nav__item', {duration: 1, opacity: 0, delay: 3.5, stagger: .2})
 
 gsap.from('.header__text', {duration: 2.5, x: '-100%', ease: "power2.out"})
 
-gsap.fromTo('.btn', {opacity: 0, sacle: 0, rotation: 360}, {duration: .8, delay: 2.5, opacity: 1, sacle: 1, rotation: 0})
+gsap.fromTo('.btn', {opacity: 0, scale: 0, rotation: 360}, {duration: .8, delay: 2.5, opacity: 1, scale: 1, rotation: 0})
 
 /* ----------------------------------------- Carousel ---------------------------------------- */
-let IndiCount = 0;
-let TeamCount = 0;
-const individualtargets = document.querySelectorAll(".work__box");
-const teamtargets = document.querySelectorAll(".project__box");
+const inditargets = document.querySelectorAll(".indi_box");
+const teamtargets = document.querySelectorAll(".team_box");
 
-function initSetting(targets) {
+function indi (targets, type) {
+  let count = 0;
+
+  const currentId = document.getElementById(`${type}-current`);
+
   gsap.set(targets, { x: '100%' });
   gsap.set(targets[0], { x: '0' });
-}
+  currentId.innerHTML = `( 1 / ${targets.length} )`;
 
-function init() {
-  initSetting(individualtargets);
-  initSetting(teamtargets);
-}
-init();
-
-// Next button
-function slideOneNext(targets, count) {
-
-gsap.fromTo(targets[count], { x: '0' }, { x: '-100%', duration: 1.2, zindex: 0 });
-count = count < targets.length - 1 ? ++count : 0;
-gsap.fromTo(targets[count], { x: '100%' }, { x: '0', delay: 0.4, duration: 1.2, zindex: 10  });
-}
-
-document.getElementById("IndiNextButton").addEventListener("click", function() {
-    slideOneNext(individualtargets, IndiCount);
-});
-document.getElementById("TeamNextButton").addEventListener("click", function() {
-  slideOneNext(teamtargets, TeamCount);
-});
-
-
-// Previous button
-function slideOnePrev(targets, count) {
-  gsap.fromTo(targets[count], { x: '0' }, { x: '100%', duration: 1.2, zindex: 0 });
-  if (count < targets.length) {
-      if (count > 0) {
-        --count;
+  // Next button
+  function slideOneNext() {
+    // console.log(count);
+    gsap.fromTo(targets[count], { x: '0' }, { x: '-100%', duration: 1.2 });
+    if (count == targets.length -1 ) {
+      count = 0;
       } else {
-        count = targets.length -1;
+        count = count + 1;
       }   
+    gsap.fromTo(targets[count], { x: '100%' }, { x: '0', delay: 0.4, duration: 1.2 });
+    currentId.innerHTML = `( ${count+1} / ${targets.length} )`;
   }
-  gsap.fromTo(targets[count], { x: '-100%' }, { x: '0', delay: 0.4, duration: 1.2, zindex: 10  });
+
+  document.getElementById(`${type}NextButton`).addEventListener("click", slideOneNext);
+
+  // Previous button
+  function slideOnePrev() {
+    // console.log(count);
+    gsap.fromTo(targets[count], { x: '0' }, { x: '100%', duration: 1.2 });
+    if (count < targets.length) {
+        if (count > 0) {
+          count = count -1;
+        } else {
+          count = targets.length -1;
+        }   
+    }
+    gsap.fromTo(targets[count], { x: '-100%' }, { x: '0', delay: 0.4, duration: 1.2 });
+    currentId.innerText = `( ${count+1} / ${targets.length} )`;
+  }
+
+  document.getElementById(`${type}PrevButton`).addEventListener("click", slideOnePrev);
 }
 
-document.getElementById("IndiPrevButton").addEventListener("click", function() {
-  slideOnePrev(individualtargets, IndiCount);
-});
-document.getElementById("TeamPrevButton").addEventListener("click", function() {
-  slideOnePrev(teamtargets, TeamCount);
-});
+indi (inditargets, "indi");
+indi (teamtargets, "team");
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("TeamNextButton").addEventListener("click", function() {
+//   slideOneNext(teamtargets, TeamCount);
+// });
+// const teamtargets = document.querySelectorAll(".project__box");
+// document.getElementById("TeamPrevButton").addEventListener("click", slideOnePrev);
