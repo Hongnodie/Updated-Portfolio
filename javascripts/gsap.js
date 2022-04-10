e6c11c141d03a8672d23774a7071caa3b81a7965
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-04-10 21:01:45 +0930

    2nd team project added

--- javascripts/gsap.js
+++ javascripts/gsap.js
@@ -7,23 +7,41 @@ gsap.from('.header__text', {duration: 2.5, x: '-100%', ease: "power2.out"})
 
 gsap.fromTo('.btn', {opacity: 0, sacle: 0, rotation: 360}, {duration: .8, delay: 2.5, opacity: 1, sacle: 1, rotation: 0})
 
-let count = 0;
-const targets = document.querySelectorAll(".work__box");
-gsap.set(targets, { x: '100%' });
-gsap.set(targets[0], { x: '0' });
+/* ----------------------------------------- Carousel ---------------------------------------- */
+let IndiCount = 0;
+let TeamCount = 0;
+const individualtargets = document.querySelectorAll(".work__box");
+const teamtargets = document.querySelectorAll(".project__box");
+
+function initSetting(targets) {
+  gsap.set(targets, { x: '100%' });
+  gsap.set(targets[0], { x: '0' });
+}
+
+function init() {
+  initSetting(individualtargets);
+  initSetting(teamtargets);
+}
+init();
 
 // Next button
-function slideOneNext() {
+function slideOneNext(targets, count) {
+
 gsap.fromTo(targets[count], { x: '0' }, { x: '-100%', duration: 1.2, zindex: 0 });
 count = count < targets.length - 1 ? ++count : 0;
 gsap.fromTo(targets[count], { x: '100%' }, { x: '0', delay: 0.4, duration: 1.2, zindex: 10  });
 }
-document.getElementById("nextButton").addEventListener("click", function() {
-    slideOneNext();
+
+document.getElementById("IndiNextButton").addEventListener("click", function() {
+    slideOneNext(individualtargets, IndiCount);
+});
+document.getElementById("TeamNextButton").addEventListener("click", function() {
+  slideOneNext(teamtargets, TeamCount);
 });
 
+
 // Previous button
-function slideOnePrev() {
+function slideOnePrev(targets, count) {
   gsap.fromTo(targets[count], { x: '0' }, { x: '100%', duration: 1.2, zindex: 0 });
   if (count < targets.length) {
       if (count > 0) {
@@ -34,6 +52,10 @@ function slideOnePrev() {
   }
   gsap.fromTo(targets[count], { x: '-100%' }, { x: '0', delay: 0.4, duration: 1.2, zindex: 10  });
 }
-document.getElementById("prevButton").addEventListener("click", function() {
-  slideOnePrev();
+
+document.getElementById("IndiPrevButton").addEventListener("click", function() {
+  slideOnePrev(individualtargets, IndiCount);
+});
+document.getElementById("TeamPrevButton").addEventListener("click", function() {
+  slideOnePrev(teamtargets, TeamCount);
 });
